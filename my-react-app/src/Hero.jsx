564d87d5f2300b267dd3b2e4d2445cdc2ae92c00
Taekjin Jung <nabaekjo2015@gmail.com>
2024-11-07 22:46:33 -0800

    added login-TJ

---
+++ my-react-app/src/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Welcome to Sentry Sight</h1>
+      <p>Your security is our priority.</p>
+      <button>Get Started</button>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ my-react-app/src/login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>This is the Login page.</p>
+    </div>
+  );
+}
+
+export default Login;
