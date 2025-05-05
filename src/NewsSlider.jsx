7b49cd23c614403fb9a7a00c4dfe61243e2c8934
Kaikuya <98040508+Kaikuya@users.noreply.github.com>
2025-05-04 20:22:05 -0700

    css integration (#84)

--- src/NewsSlider.jsx
+++ src/NewsSlider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import './newsSlider.css';
+import './homepage.css';
 
 const NewsSlider = () => {
   const [news, setNews] = useState([]);
